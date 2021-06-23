fd84db1b4e2fba0edf8b3e9e2b010f68e9c7ae50
Mark-Vale <markpvale@gmail.com>
2021-06-22 22:26:04 -0400

    config

---
+++ db/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.connect('mogodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  // we're connected!
+});
+
